eb68fa6550564493a128c10a35caa94791bc22bb
ritish <sritish.10@gmail.com>
2022-07-18 22:32:51 +0530

    updates

--- create_schemas.py
+++ create_schemas.py
@@ -4,48 +4,46 @@
 
 # mycursor = mydb.cursor()
 
-# create customer_details table
-# mycursor.execute('CREATE TABLE CUSTOMER_DETAILS (NAME VARCHAR(20), USERNAME VARCHAR(20) PRIMARY KEY, PIN INT, BALANCE INT)')
+# # create customer_details table
+# # mycursor.execute('CREATE TABLE CUSTOMER_DETAILS (NAME VARCHAR(20), USERNAME VARCHAR(20) PRIMARY KEY, PIN INT, BALANCE INT)')
 
-# insert_customers = 'INSERT INTO CUSTOMER_DETAILS (NAME, USERNAME, PIN, BALANCE) VALUES (%s, %s, %s, %s)'
-# customer_details = [
+# # insert_customers = 'INSERT INTO CUSTOMER_DETAILS (NAME, USERNAME, PIN, BALANCE) VALUES (%s, %s, %s, %s)'
+# # customer_details = [
 
-#         ('Santhosh Kumar', '20071A6620', 111111, 1000),
-#         ('Sai Ritish', '20071A6626', 654321, 1000),
-#         ('Uday Krishna', '20071A6627', 999999, 1000),
-#         ('Harikesh', '20071A6629', 123456, 1000)
+# #         ('Santhosh Kumar', '20071A6620', 111111, 1000),
+# #         ('Sai Ritish', '20071A6626', 654321, 1000),
+# #         ('Uday Krishna', '20071A6627', 999999, 1000),
+# #         ('Harikesh', '20071A6629', 123456, 1000)
 
-#     ]
+# #     ]
 
-# mycursor.executemany(insert_customers, customer_details)
+# # mycursor.executemany(insert_customers, customer_details)
 
-# mydb.commit()
+# # mydb.commit()
 
-#creating Stalls Tables
+# # # creating Stalls Tables
 
-# stall-FastFood
+# # # stall-FastFood
 
-# create_table = "CREATE TABLE FastFood (FOOD_ITEM VARCHAR(20), PRICE INT)"
-# mycursor.execute(create_table)
+# # create_table = "CREATE TABLE FastFood (FOOD_ITEM VARCHAR(20), PRICE INT)"
+# # mycursor.execute(create_table)
 
-# insert_food_data = "INSERT INTO FastFood (FOOD_ITEM, PRICE) VALUES(%s, %s)"
-# food_data = [
+# # insert_food_data = "INSERT INTO FastFood (FOOD_ITEM, PRICE) VALUES(%s, %s)"
+# # food_data = [
 
-#         ('Veg Manchuria', 40),
-#         ('Egg Manchuria', 50),
-#         ('Egg Fried Rice', 40),
-#         ('Manchuria Rice',	50),
-#         ('Egg Noodles',	40),
-#         ('Manchuria Noodles', 50)
+# #         ('Veg Manchuria', 40),
+# #         ('Egg Manchuria', 50),
+# #         ('Egg Fried Rice', 40),
+# #         ('Manchuria Rice',	50),
+# #         ('Egg Noodles',	40),
+# #         ('Manchuria Noodles', 50)
         
-#     ]
+# #     ]
 
-# mycursor.executemany(insert_food_data, food_data)
+# # mycursor.executemany(insert_food_data, food_data)
 
-# mydb.commit()
+# # mydb.commit()
 
-
-# mycursor.execute('ALTER TABLE CUSTOMER_DETAILS ADD BALANCE INT')
 
 # create_table = "CREATE TABLE ChillZone (FOOD_ITEM VARCHAR(20), PRICE INT)"
 # mycursor.execute(create_table)
@@ -112,7 +110,7 @@
 #         "AMOUNT INT)"
 #     )
 
-# create stall orders table
+# # create stall orders table
 
 # mycursor.execute(
         
